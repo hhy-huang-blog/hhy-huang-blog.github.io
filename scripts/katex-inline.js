1fbf9dfeddec7a65cfd4442bedbbe9f7dfe1f17c
haoyuhuang <haoyuhuang@bjtu.edu.cn>
2026-06-26 20:23:34 +0800

    up

---
+++ scripts/katex-inline.js
@@ -0,0 +1,14 @@
+'use strict';
+
+hexo.extend.injector.register('head_end', () => `
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.css">
+<script defer src="https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.js"></script>
+<script defer src="https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/contrib/auto-render.min.js"
+  onload="renderMathInElement(document.body, {
+    delimiters: [
+      {left: '$$', right: '$$', display: true},
+      {left: '$', right: '$', display: false}
+    ],
+    throwOnError: false
+  });"></script>
+`);
